e367f87f1a4e0928d95cdc07d188643a3f3b1b3b
unknown <rahmanmahadi966@gmail.com>
2021-10-04 14:27:49 +0600

    readme

--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -1,42 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const MenuBar = () => {
     return (
-    //     <div>
-    //      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    //   <div className="container">
-    //     <div className="row">
-    //       <div className="col-md-2">
-    //         <div className="logo-img">
-    //           <h1>Learn<span className="text-info">Bridge</span></h1>
-    //         </div>
-    //       </div>
-    //       <div className="col-md-10">
-    //         <div className="menu-container ">
-    //           <ul className="navbar-nav">
-    //             <Link to="/home" className="nav-item">
-    //               <li>Home</li>
-    //             </Link>
-    //             <Link to="/about" className="nav-item">
-    //               <li>About us</li>
-    //             </Link>
-    //             <Link to="/courses" className="nav-item">
-    //               <li>Courses</li>
-    //             </Link>
-    //             <Link to="/faq" className="nav-item">
-    //               <li>FAQ</li>
-    //             </Link>
-    //             <Link to="/contact" className="nav-item">
-    //               <li>Contact us</li>
-    //             </Link>
-    //           </ul>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </nav>
-    //     </div>
+   <div className="menubar">
+  
 
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
 <div className="container-fluid">
@@ -46,28 +14,29 @@ const MenuBar = () => {
   </button>
   <div className="collapse navbar-collapse" id="navbarNavDropdown">
     <ul className="navbar-nav ms-auto">
-        <Link to="/home" className="nav-item text-decoration-none p-4">
+        <NavLink to="/home" activeStyle={{ color: 'black', fontSize: '1.3rem' }} className="nav-item text-decoration-none p-4 align-items-center">
               <li>Home</li>
-        </Link>
-        <Link to="/about" className="nav-item  text-decoration-none p-4">
+        </NavLink>
+        <NavLink to="/about" activeStyle={{ color: 'black', fontSize: '1.3rem' }} className="nav-item  text-decoration-none p-4">
               <li>About us</li>
-        </Link>
-        <Link to="/courses" className="nav-item text-decoration-none p-4">
+        </NavLink>
+        <NavLink to="/courses" activeStyle={{ color: 'black', fontSize: '1.3rem' }} className="nav-item text-decoration-none p-4">
               <li>Courses</li>
-        </Link>
-        <Link to="/faq" className="nav-item text-decoration-none p-4">
+        </NavLink>
+        <NavLink to="/faq" activeStyle={{ color: 'black', fontSize: '1.3rem'}} className="nav-item text-decoration-none p-4">
               <li>FAQs</li>
-        </Link>
-        <Link to="/contact" className="nav-item text-decoration-none p-4">
+        </NavLink>
+        <NavLink to="/contact" activeStyle={{ color: 'black', fontSize: '1.3rem' }} className="nav-item text-decoration-none p-4">
               <li>Contact us</li>
-        </Link>
-        <Link to="/contact" className="nav-item text-decoration-none p-4">
+        </NavLink>
+        <NavLink to="/contact" className="nav-item text-decoration-none p-4">
               <li className="btn btn-primary">Sign Up</li>
-        </Link>
+        </NavLink>
     </ul>
   </div>
 </div>
 </nav>
+</div>
     );
 };
 
